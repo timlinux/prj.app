67709eb2d40f488d029d3436f380735e5d38da15
Tim Sutton <tim@kartoza.com>
2017-10-25 13:28:19 +0200

    Move nginx container related stuff ot its own repo, build with that image

--- deployment/build_production_image.sh
+++ deployment/build_production_image.sh
@@ -37,6 +37,11 @@ then
 	# Clean up
 	rm -rf docker/django_project.tar.gz docker/Dockerfile-prod
     rm docker/Dockerfile-prod
+
+    #
+    # Now build our nginx image
+    # 
+
 else
 	echo "Production image build aborted."
 fi
